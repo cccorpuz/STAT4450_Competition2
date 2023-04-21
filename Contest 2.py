28af81f8215d83ba90067946ffab03af3000ae14
joshgryzen <58818027+joshgryzen@users.noreply.github.com>
2023-04-21 18:51:00 -0500

    created pred

--- Contest 2.py
+++ Contest 2.py
@@ -17,6 +17,7 @@ train = pd.read_csv("./train.csv/train.csv")
 test = pd.read_csv("./test.csv/test.csv")
 
 # 1. Removing fake results from test dataframe
+test_codes = test["ID_code"].values
 test.drop(["ID_code"], axis=1, inplace=True)
 test = test.values
 
@@ -28,6 +29,8 @@ for i in tqdm(range(test.shape[1])):
 real_idx = np.argwhere(np.sum(unique_count, axis=1) > 0)[:, 0]
 synth_idx = np.argwhere(np.sum(unique_count, axis=1) == 0)[:, 0]
 
+test_length = test.shape[0]
+test_synth = test[synth_idx, :]
 test = test[real_idx, :]
 
 # 2. Append real test to training
@@ -36,6 +39,7 @@ train_length = train.shape[0]
 train.drop(["ID_code"], axis=1, inplace=True)
 train.drop(["target"], axis=1, inplace=True)
 full = pd.DataFrame(np.concatenate([train.values, test]), columns=train.columns)
+synth = pd.DataFrame(test_synth, columns=train.columns)
 print(full.shape)
 
 # Reverse features with negative correlation
@@ -49,47 +53,62 @@ sigma_fac = 0.001
 sigma_base = 4
 eps = 0.00000001
 
-# Get counts
-features_count = np.zeros((full.shape[0], len(features)))
-features_density = np.zeros((full.shape[0], len(features)))
-features_deviation = np.zeros((full.shape[0], len(features)))
 
-sigmas = []
-for i, var in enumerate(tqdm(features)):
-    full_int = (full[var].values * 10000).round().astype(int)
-    low = full_int.min()
-    full_int -= low
-    high = full_int.max() + 1
-    counts_full = np.bincount(full_int, minlength=high).astype(float)
+def get_features(data):
+    # Get counts
+    features_count = np.zeros((data.shape[0], len(features)))
+    features_density = np.zeros((data.shape[0], len(features)))
+    features_deviation = np.zeros((data.shape[0], len(features)))
 
-    # Geometric mean of twice sigma_base and a sigma_scaled which is scaled to the length of array
-    sigma_scaled = counts_full.shape[0] * sigma_fac
-    sigma = np.power(sigma_base * sigma_base * sigma_scaled, 1 / 3)
-    sigmas.append(sigma)
-    counts_full_smooth = scipy.ndimage.filters.gaussian_filter1d(counts_full, sigma)
-    deviation = counts_full / (counts_full_smooth + eps)
-    indices = full_int
-    features_count[:, i] = counts_full[indices]
-    features_density[:, i] = counts_full_smooth[indices]
-    features_deviation[:, i] = deviation[indices]
+    sigmas = []
+    for i, var in enumerate(tqdm(features)):
+        data_int = (data[var].values * 10000).round().astype(int)
+        low = data_int.min()
+        data_int -= low
+        high = data_int.max() + 1
+        counts_data = np.bincount(data_int, minlength=high).astype(float)
 
-features_count_names = [var + "_count" for var in features]
-features_density_names = [var + "_density" for var in features]
-features_deviation_names = [var + "_deviation" for var in features]
+        # Geometric mean of twice sigma_base and a sigma_scaled which is scaled to the length of array
+        sigma_scaled = counts_data.shape[0] * sigma_fac
+        sigma = np.power(sigma_base * sigma_base * sigma_scaled, 1 / 3)
+        sigmas.append(sigma)
+        counts_data_smooth = scipy.ndimage.filters.gaussian_filter1d(counts_data, sigma)
+        deviation = counts_data / (counts_data_smooth + eps)
+        indices = data_int
+        features_count[:, i] = counts_data[indices]
+        features_density[:, i] = counts_data_smooth[indices]
+        features_deviation[:, i] = deviation[indices]
 
-full_count = pd.DataFrame(columns=features_count_names, data=features_count)
-full_count.index = full.index
-full_density = pd.DataFrame(columns=features_density_names, data=features_density)
-full_density.index = full.index
-full_deviation = pd.DataFrame(columns=features_deviation_names, data=features_deviation)
-full_deviation.index = full.index
-full = pd.concat([full, full_count, full_density, full_deviation], axis=1)
+    features_count_names = [var + "_count" for var in features]
+    features_density_names = [var + "_density" for var in features]
+    features_deviation_names = [var + "_deviation" for var in features]
 
-features_count = features_count_names
-features_density = features_density_names
-features_deviation = features_deviation_names
+    data_count = pd.DataFrame(columns=features_count_names, data=features_count)
+    data_count.index = data.index
+    data_density = pd.DataFrame(columns=features_density_names, data=features_density)
+    data_density.index = data.index
+    data_deviation = pd.DataFrame(
+        columns=features_deviation_names, data=features_deviation
+    )
+    data_deviation.index = data.index
+    data = pd.concat([data, data_count, data_density, data_deviation], axis=1)
 
+    features_count = features_count_names
+    features_density = features_density_names
+    features_deviation = features_deviation_names
+
+    return data, features_count, features_density, features_deviation
+
+
+full, features_count, features_density, features_deviation = get_features(full)
+(
+    synth,
+    fake_features_count,
+    fake_features_density,
+    fake_features_deviation,
+) = get_features(synth)
 print(full.shape)
+print(f"test_synth: {synth.shape}")
 
 # Standardizing the features
 features_to_scale = [features, features_count]
@@ -148,8 +167,21 @@ num_rounds = 100
 model = lgb.train(params, train_data, num_rounds, valid_sets=[val_data])
 
 # make predictions on the validation data
-y_pred = model.predict(x_val)
+# y_pred = model.predict(x_val)
 
 # print the accuracy
-accuracy = accuracy_score(y_val, y_pred.round().astype(int))
-print(accuracy)
+# accuracy = accuracy_score(y_val, y_pred.round().astype(int))
+# print(accuracy)
+
+# Make final prediction for test
+sub = pd.DataFrame({"ID_code": test_codes})
+
+pred_real = model.predict(test)
+pred_synth = model.predict(synth)
+
+preds_all = np.zeros(test_length)
+preds_all[real_idx] = pred_real
+preds_all[synth_idx] = pred_synth
+sub["target"] = preds_all
+sub.to_csv("submission.csv", index=False)
+print(sub.head(20))
